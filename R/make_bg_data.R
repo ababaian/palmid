65c21d9b2734a8f264ef5044635f1a37737d7835
ababaian <ababaian@bccrc.ca>
2021-09-01 16:26:25 +0100

    Add lazyload for palmdb-bg set in R

--- R/make_bg_data.R
+++ R/make_bg_data.R
@@ -2,30 +2,41 @@
 #' Read a multiple-FEV file to create a background set of palmprints
 #' Standard approach is to use palmDB 
 #' @param fev.path Path to multiple fev file
-#' @return NULL: will write an RDS file to <fev.path>.RDS
+#' @param dataset.id Name for output dataset.
+#' @return NULL: will write an Rdata file to data/<fev.path>.Rdata
 #' @keywords palmid palmdb palmprints
 #' @examples
 #' # Download palmDB to make background set on
 #' system("git clone https://github.com/rcedgar/palmdb.git")
 #' # Generate FEV with palmscan
-#' system("palmscan -search_pp palmdb/2021-03-02/otu_centroids.fa -all -rdrp -fevout R/palmdb210302.fev")
+#' system("palmscan -search_pp palmdb/2021-03-02/otu_centroids.fa -all -rdrp -fevout data/palmdb210302.fev")
 #' # Create R object (data.frame)
-#' make_bg_data(R/palmdb210302.fev)
+#' make_bg_data(data/palmdb210302.fev)
 #' @export
-make_bg_data <- function(fev.path) {
+make_bg_data <- function(fev.path, dataset.id = NULL) {
   # Read a multiple-FEV file from palmscan
   # to create a background dataset of RdRP statistics
   # - palmprint lengths
   # - Total scores
   # - motif scores (NA)
   
-  bg.fev <- read.fev(fev.path)
+  # If dataset.id is unused, use fev-name as the variable name
+  if (is.null(dataset.id)) {
+    # strip leading path directory
+    dataset.id <- gsub(".*/","", fev.path)
+    print( paste0("Creating background dataset as ,", dataset.id))
+  }
+  
+  print( paste0("Creating background dataset as: ", dataset.id))
+  print( paste0("  saved as: data/", dataset.id, ".Rdata"))
+  
+  assign(dataset.id, read.fev(fev.path))
   
   #bg.fev <- bg.fev[, c('score', 'pssm_total_score', 'pp_length', 'v1_length', 'v2_length')]
   
-  rdata.path <- paste0(fev.path, ".RDS")
+  rdata.path <- paste0("data/", dataset.id, ".Rdata")
   
-  saveRDS(bg.fev, rdata.path)
+  save( list = dataset.id, file = rdata.path)
 }
 ## Make background data-set (run once)
 #fev.path <- 'R/palmdb210302.fev'
---
+++ R/palmdb-data.R
@@ -0,0 +1,14 @@
+#' palmdb <210302>
+#'
+#' Palmprint database - initial version (210302) containing GenBank v241
+#' RNA depenendent RNA polymerase palmprints
+#'
+#' @docType data
+#' @usage data(palmdb)
+#' @format a multiple-palmprint data.frame
+#' @keywords palmdb datasets
+#' @source \href{https://github.com/rcedgar/palmdb}{palmDB repo}
+#'
+#' @examples
+#' data(palmdb)
+"palmdb"
