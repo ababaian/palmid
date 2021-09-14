1e3f7b3340f03a6e51432b6d2a07518679726d7f
ababaian <ababaian@bccrc.ca>
2021-09-14 15:03:15 +0100

    Add geotime report to rscript

--- scripts/palmid.R
+++ scripts/palmid.R
@@ -36,8 +36,11 @@ if (length(args)==0) {
 
 input.fev     <- args[1]
 output.report <- args[2]
+
 input.pro     <- gsub('_pp.png', '.pro',     output.report)
 output.pro    <- gsub('_pp.png', '_pro.png', output.report)
+output.geo    <- gsub('_pp.png', '_geo.png', output.report)
+
 
 # SCRIPT ========================================
 
@@ -69,3 +72,20 @@ png(filename = output.pro, width = 800, height = 400)
   plot(pro.report)
 dev.off()
 
+# GEOTIME ---------------------------------------
+#------------------------------------------------
+# Establish Serratus server connection
+con <- SerratusConnect()
+
+# For each parent and child sOTU, retrieve matching SRA runs
+ppdb.hits <- pro.in$sseqid[(pro.in$pident >= 0)]
+  palm.uid  <- get.sOTU(ppdb.hits, con, get_childs = T)
+  palm.usra <- get.sra(palm.uid, con)
+
+# ANALYZE SRA/BIOSAMPLE SOURCES ---------------------------
+geo.report <- PlotGeoReport(palm.usra, con)
+
+# SAVE GEO-REPORT 
+png(filename = output.pro, width = 800, height = 500)
+  plot(geo.report)
+dev.off()
