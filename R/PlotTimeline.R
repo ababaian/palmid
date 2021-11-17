89b1ab4601a054f7f9bd5e6491ad138c441c6ac3
ababaian <ababaian@bccrc.ca>
2021-11-17 10:21:06 +0000

    Update timeline to plotly

--- R/PlotTimeline.R
+++ R/PlotTimeline.R
@@ -1,35 +1,73 @@
 #' PlotTimeline
-#' Create a timeline of
+#' Create a timeline of data-releases in palm.sra
 #'
-#' @param run_ids     character, vector of sra run_ids
-#' @param sra.dates POSIXct, set of dates
-#' @param con      pq-connection, use SerratusConnect()
+#' @param palm.sra data.frame, created with get.palmSra(pro.df, con)
+#' @param html boolean, generate htmlWidget instead of ggplot [TRUE]
 #' @return ggplot2, timeline of SRA load dates
-#' @keywords palmid sql timeline
+#' @keywords palmid timeline
 #'
 #' @import viridisLite
 #' @import dplyr ggplot2
 #' @export
 # Retrieve date from input of sra run_ids
-PlotTimeline <- function(run_ids = NULL, sra.dates = NULL, con = SerratusConnect()){
+PlotTimeline <- function(palm.sra, html = TRUE){
+  
+  # Color pallette
+  ranklvl   <-  c("phylum",  "family",  "genus",   "species")
+  rankcols  <-  c("#9f62a1", "#00cc07", "#ff9607", "#ff2a24")
+  
+  rankdraw <- setNames(c("#9f62a1", "#00cc07", "#ff9607", "#ff2a24"),
+                       c("phylum",  "family",  "genus",   "species"))
 
-  if ( is.null(run_ids) & is.null(sra.dates) ){
-    stop("One of 'run_ids' or 'sra.dates' needs to be set")
-  } else if (!is.null(run_ids)){
-    sra.date   <- get.sraDate(run_ids, con = con, as.df =  T)
-  } else if (!is.null(sra.dates)){
-    # do nothing
-  }
-
-  timeline <- ggplot( sra.date, aes(date)) +
+  palm.sra$rank <- ranklvl[1]
+  palm.sra$colr <- rankcols[1]
+  
+  palm.sra$rank[palm.sra$pident >= 45] <- ranklvl[2]
+  palm.sra$colr[palm.sra$pident >= 45] <- rankcols[2]
+  
+  palm.sra$rank[palm.sra$pident >= 70] <- ranklvl[3]
+  palm.sra$colr[palm.sra$pident >= 70] <- rankcols[3]
+  
+  palm.sra$rank[palm.sra$pident >= 90] <- ranklvl[4]
+  palm.sra$colr[palm.sra$pident >= 90] <- rankcols[4]
+  
+  
+  if (html) {
+  timeline <- plotly::plot_ly(x = palm.sra$date[ palm.sra$rank == 'species'],
+                      color = ranklvl[4], colors = rankdraw,
+                      type = 'histogram', bingroup = 1,
+                      nbinsx  = 120,
+                      xbins = list( start = as.POSIXct("2010-01-01"),
+                                    end   = as.POSIXct("2021-01-02"))) %>%
+    plotly::add_trace(x = palm.sra$date[ palm.sra$rank == 'genus'],
+                        color = ranklvl[3],
+                      type = 'histogram', bingroup = 1) %>%
+    plotly::add_trace(x = palm.sra$date[ palm.sra$rank == 'family'],
+                        color = ranklvl[2],
+                        type = 'histogram', bingroup = 1) %>%
+    plotly::add_trace(x = palm.sra$date[ palm.sra$rank == 'phylum'],
+                        color = ranklvl[1],
+                        type = 'histogram', bingroup = 1) %>%
+    plotly::layout(barmode = "overlay", bargap = 0.1,
+           yaxis = list( title = 'SRA count'),
+           xaxis = list( title = 'release date',
+                         autorange = TRUE,
+                         range = list( as.POSIXct( c("2010-01-01","2021-01-02") )))) %>%
+    plotly::config(displaylogo = FALSE)
+  
+  } else {
+  timeline <- ggplot( palm.sra, aes(date, fill = colr)) +
     geom_histogram(bins = 120) +
+    scale_fill_identity() +
     scale_x_datetime(date_breaks = "2 years",
                      date_minor_breaks = "1 month",
                      date_labels = "%Y",
                      limits = as.POSIXct( c("2010-01-01","2021-01-02")) ) +
     xlab("Run release date") + ylab("runs") +
+    #facet_wrap(ncol = 1, scales = "free_y", ~rank) +
     theme_classic()
-
+  }
+  
   return(timeline)
-
+  
 }
