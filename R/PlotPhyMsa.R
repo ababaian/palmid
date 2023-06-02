8ddb2eb8328b7c4fb349cf059219f57ddf532967
ababaian <artem@rRNA.ca>
2023-06-02 14:20:55 -0400

    Change waxsys example msa to data file, not path

--- R/PlotPhyMsa.R
+++ R/PlotPhyMsa.R
@@ -2,12 +2,12 @@
 #' A multi-plot wrapper to generate an MSA plot from input.msa and
 #' align it with a phylogenetic tree plot.
 #'
-#' @param input.msa   msa file path relative to .fev
+#' @param input.msa   msa file path relative OR AAStringSet MSA object
 #' @param p phylogeny visualization object. use PlotPhy
 #' @return A single object consisting of two facets for tree and MSA
 #' @keywords dplyr ggplot2 treeio ggtree msa
 #' @examples
-#' data("waxsys.input.msa")
+#' data("waxsys.msa")
 #' data("waxsys.tree.phy")
 #' p <- PlotPhy(waxsys.tree.df, waxsys.tree.phy)
 #' plotPhyMsa <- PlotPhyMsa(waxsys.input.msa, p)
@@ -19,8 +19,12 @@
 PlotPhyMsa <- function(input.msa, p) {
     requireNamespace("ggmsa", quietly = T)
     requireNamespace("ggtree", quietly = T)
-
-    aa_string <- readAAStringSet(input.msa)
+    
+    if ( is.character(input.msa) ){
+      aa_string <- readAAStringSet(input.msa)
+    } else if ( is.object(input.msa) ){
+      aa_string <- input.msa
+    }
     
     taxa_order = get_taxa_name(p)
     aa_df <- data.frame(
---
+++ R/waxsys.msa.R
@@ -0,0 +1,15 @@
+#' waxsys.msa
+#'
+#' Waxsystermes Virus example data. MUSCLE-generated multiple sequence
+#' alignment of Waxsystermes example data. File imported with
+#' Biostrings::readAAStringSet("inst/extdata/waxsys.msa.fa")
+#'
+#'
+#' @name waxsys.msa
+#' @usage data(waxsys.msa)
+#' @docType data
+#' @format AAStringSet with 11 entries
+#' @keywords waxsystermes-example datasets
+#' @source \href{https://github.com/ababaian/serratus/wiki/Find_novel_viruses_B_palmdb}{Waxsystermes Virus Tutorial}
+#'
+NULL
